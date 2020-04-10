4f022d84d8a4b6f34758ce562b8d2e69fe9e0bd1
lilyackroyd <lilyackroyd@gmail.com>
2020-04-10 17:26:59 +0100

    styling and tweaks to faqs page - added links

--- Views/pages/faq.php
+++ Views/pages/faq.php
@@ -1,23 +1,8 @@
-<!DOCTYPE html>
-<html>
-<head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
+
 <style>
-body {
-  font-family: Arial, Helvetica, sans-serif;
-  margin: 0;
-}
-html{
-    box-sizing: border-box;
-}
 
 
-.Frequently Asked Questions {
-  padding: 50px;
-  text-align: center;
-  background-color: #474e5d;
-  color: white;
-}
+
 .container {
   padding: 0 16px;
 }
@@ -28,9 +13,7 @@ html{
   display: table;
 }
 
-.title {
-  color: grey;
-}
+
 .button {
   border: none;
   outline: 0;
@@ -43,68 +26,57 @@ html{
   width: 100%;
 }
 
-
-
-.How do I register {
-  background-color: #ffdddd;
-  border-left: 6px solid #f44336;
+.questions{
+        padding-left:80px;
+    padding-right:84px;
+    width:100%;
+}
+.question{
+    padding-bottom: 20px;
 }
 
-.How do I subscribe {
-  background-color: #ddffdd;
-  border-left: 6px solid #4CAF50;
-}
-
-.How can I become a blogger {
-  background-color: #e7f3fe;
-  border-left: 6px solid #2196F3;
-}
-
-
-.Can I publish my blog in your website{
-  background-color: #ffffcc;
-  border-left: 6px solid #ffeb3b;
-}
 p.double {border-style: double;
 } 
 
 </style>
-</head>
-<body>
-     <div class="header Frequently Asked Questions">
-  <h1>Frequently Asked Questions</h1>
- 
+
+
+  <section class="intro-section">
+  <h1>Frequently asked questions</h1>
+  </section>  
+
+<div class="questions">
+
+<div class="question">
+  <strong>1. How do I register as a blogger? </strong> <p>
+      You can register as a blogger <a href="?controller=user&action=register"/>here</a>. Fill in your details and log in. It's as simple as that!</p>
 </div>
 
-<div class="How do I register">
-  <p><strong>1. How do I register? </strong> 
-      Go to our contact us page. Click on sign in and fill in your details. It is as simple as.</p>
+<div class="question">
+  <strong>2. How do I subscribe?</strong><p>You can subscribe to The Awesome Isolation blog here. Fill in your details and log in.</p>
 </div>
 
-<div class="How do I subscribe">
-  <p><strong>2. How do I subscribe?</strong> Go to our contact us page. Click on subscribe in and fill in your details. It is as simple as..</p>
+    <div class="question">
+  <strong>3. Where's my account? </strong> <p>
+      If you're a blogger for The Awesome Isolation Blog you can log in using your username and password <a href="?controller=user&action=register"/>here</a>.</p>
 </div>
 
-<div class="How can I become a blogger">
-  <p><strong>3. How can I become a blogger? </strong> Go to our contact us page. 
-      Click on to sign up link; fill in your details to register with your credentials.</p>
-</div>
-
-<div class="Can I publish my blog on your website">
-  <p><strong>4. Can I publish my blog on your website?</strong> Yes you can publish your blog on our site. 
+<div class="question">
+ <strong>3. Can I publish my blog on your website?</strong> <p> Yes you can publish your blog on our site. 
       Go to our contact page to register your credentials. </p>
 </div>
-<div class="How much is your subscription charge">
-  <p><strong>5. How much is your subscription charge?</strong> Our subscription charge is £4.99 a month. </p>
+<div class="question">
+  <strong>4. How much is your subscription charge?</strong><p> Our subscription charge is £4.99 a month. </p>
 </div>
 
-<div class="How much do you charge to pubish a post on your site">
-    <p><strong>6. How much do you charge to publish a post on your site?</strong> Our publication charge is £39.99 a month. </p>
+<div class="question">
+    <strong>5. How much do you charge to publish a post on your site?</strong><p> Our publication charge is £39.99 a month. </p>
+ </div>   
     
-    
-<div class="How can I cancel my subscription account">    
-<p><strong>7. How can I cancel my subscription account?</strong> We're sorry you want to cancel your account.
-               Select the Cancel subscription button on the Membership panel in My Account. </p>
+<div class="question">    
+<strong>6. How can I edit my details?</strong> 
+<p>If you want to update any of your details, just <a href="?controller=user&action=login"/>log in</a> and go to the 'My details section.'</p>
 </div>
-</body>
-</html>
+
+</div>
+
