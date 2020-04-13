085c76abbfd338ce3501d53d00520d5c6ffc2f82
SarahSanderson <sarah21sanderson@gmail.com>
2020-04-13 12:59:29 +0100

    more admin bits, still not finished......

--- Views/users/admin.php
+++ Views/users/admin.php
@@ -2,29 +2,7 @@
 $bloggers = userController::bloggerlist();
 $subscribers = userController::subscriberList();
 ?> <br>
- <?php if (count($bloggers) > 0) { ?>
- <?php foreach ($bloggers as $blogger) { ?> <br>
-                  
-<?php echo $blogger->firstname . ' '. $blogger->lastname; ?>
-                    <?php
-                }
-                }
-            else {
-                echo '<p>There are currently no bloggers. </p>';
-            }
-            ?>
  
- <?php if (count($subscribers) > 0) { ?>
- <?php foreach ($subscribers as $subscriber) { ?> <br>
-                  
-<?php echo $subscriber->firstname . ' '. $subscriber->lastname; ?>
-                    <?php
-                }
-                }
-            else {
-                echo '<p>There are currently no subscribers. </p>';
-            }
-            ?>
 
                 <div class="table-container-intro-account" role="table" aria-label="">
                     <div class="flex-table-account row" role="rowgroup">  
@@ -39,4 +17,95 @@ $subscribers = userController::subscriberList();
                         </div> 
                         
                   </div>
+                    
+                   
+                    
                 </div>
+
+ 
+ <div class="container">
+  <div class="row">
+      <?php if (count($bloggers) > 0) { ?>
+         <h2>List of Bloggers</h2>
+             <table class="table">
+                                  <thead class="thead-dark">
+                                    <tr>
+                                      <th scope="col">Image</th>
+                                      <th scope="col">First name</th>
+                                      <th scope="col">Last name</th>
+                                      <th scope="col">Delete?</th>
+                                    </tr>
+                                  </thead>
+                                  <tbody>
+                                       <?php foreach ($bloggers as $blogger) { ?> 
+                                    <tr>
+                                      <th scope="row"><?php echo $blogger->img; ?></th>
+                                      <td><?php echo $blogger->firstname; ?></td>
+                                      <td><?php echo $blogger->lastname; ?></td>
+                                      <td><?php echo $blogger->id; ?></td>
+                                    </tr>
+                                    
+                                
+                   <?php
+                }
+                
+                ?> 
+                       </tbody>
+                                </table>   
+                        
+                <?php }
+            else {
+                echo '<p>There are currently no bloggers. </p>';
+            }
+            ?> 
+                                
+
+                     </div><!-- end of row -->
+                     
+                     
+                     
+ <div class="row">
+      <?php if (count($subscribers) > 0) { ?>
+         <h2>List of Subscribers</h2>
+             <table class="table">
+                                  <thead class="thead-dark">
+                                    <tr>
+                                      <!--<th scope="col">Image</th>-->
+                                      <th scope="col">First name</th>
+                                      <th scope="col">Last name</th>
+                                      <th scope="col">Delete?</th>
+                                    </tr>
+                                  </thead>
+                                  <tbody>
+                                       <?php foreach ($subscribers as $subscriber) { ?> 
+                                    <tr>
+                                      <!--<th scope="row"><?php echo $subscriber->img; ?></th>-->
+                                      <td><?php echo $subscriber->firstname; ?></td>
+                                      <td><?php echo $subscriber->lastname; ?></td>
+                                      <td><?php echo $subscriber->id; ?></td>
+                                    </tr>
+                                    
+                                
+                   <?php
+                }
+                
+                ?> 
+                       </tbody>
+                                </table>   
+                        
+                <?php }
+            else {
+                echo '<p>There are currently no bloggers. </p>';
+            }
+            ?> 
+                                
+
+                     </div><!-- end of row -->
+                     
+                     
+                     
+                     
+     </div><!-- end of container -->
+                    
+ 
+ 
