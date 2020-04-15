bf2c9fc7dd8e899b61c05a02f136d06e6b3881a1
Claire Winterbottom <clairemwinterbottom@gmail.com>
2020-04-15 20:05:12 +0100

    Updated error page

--- Views/pages/error.php
+++ Views/pages/error.php
@@ -1,5 +1,13 @@
 <section class="intro-section">
-<h1>error page!!!</h1>    
+         <section class="main-section">
+                    <div class="w3-container w3-red">
+                        <h3>Oops, it looks like something went wrong.</h3>
+                        <img src="Views/images/error.jpg" width="500" />
+                        
+                    <p> Click <a href="?controller=blog&action=home">here </a> to return to the home page while we hang our heads and think about what we've done.</p>
+                    </div>
+                </section>
+       
 </section>
 
 
